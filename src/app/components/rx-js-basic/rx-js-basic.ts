0abb2d8d98649a25f73f83e4aa7242bcd5edb7f0
Github Projects <barik011@gmail.com>
2026-01-08 19:46:57 +0530

    Subject and behaviourSubject

--- src/app/components/rx-js-basic/rx-js-basic.ts
+++ src/app/components/rx-js-basic/rx-js-basic.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { from, interval, Observable, of } from 'rxjs';
+import { BehaviorSubject, from, interval, Observable, of, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-rx-js-basic',
@@ -8,22 +8,25 @@ import { from, interval, Observable, of } from 'rxjs';
   styleUrl: './rx-js-basic.css',
 })
 export class RxJsBasic {
+
+    //Uni Cast (Cold Observable) Observable
     $obsrData = new Observable<number>(res=>{
       res.next(123);
     });
-
     $fullName = new Observable<string>(fullname=>{
       fullname.next('Mohammad');
     });
-     
-
     $count= interval(5000);
-
     $empData =  of({empName:'Barique',empCity:'Purnea',empState:'Bihar',empMob: 9898989898});
     $areaCode =  of(['110025','124514','5465865','5465875']);
-
-
     $cityList = from(['Purnea','katihar','Araria','Kisanganj']);
+
+
+    //Multi Cast (Hot Observable)
+
+    $loggedUserNameSub:Subject<string>= new Subject<string>;
+
+    $loggedUserRole:BehaviorSubject<string> = new BehaviorSubject<string>("Admin");
 
 
 
@@ -60,6 +63,24 @@ export class RxJsBasic {
       debugger;
       console.log(city);
     })
+    let finalValue
+    this.$loggedUserNameSub.subscribe((res)=>{
+      debugger;
+      finalValue= res;
+    })
+
+    this.$loggedUserNameSub.next('Barique');
+    this.$loggedUserNameSub.next('Chandni');
+    this.$loggedUserNameSub.next('Saad');
+    this.$loggedUserNameSub.next('Adeen');
+
+    console.log(finalValue)
+
+    this.$loggedUserRole.subscribe((res)=>{
+      debugger;
+    })
+
+    this.$loggedUserRole.next('Super Admin');
 
 
     }
